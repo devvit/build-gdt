c80bada3a25c166ef4edfd00358eb6c5ca63d958
ymca <admin@example.com>
2024-01-22 00:00:04 +0800

    Mon Jan 22 00:00:02 CST 2024  M .github/workflows/macos_editor_3.yml  M build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #
 
+export JAVA_HOME=$JAVA_HOME_17_X64
+scons platform=android target=release android_arch=armv7
+scons platform=android target=release android_arch=arm64v8
 cd platform/android/java
 ./gradlew generateGodotTemplates
