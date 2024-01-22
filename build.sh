366aca0205d091ddfad7a860834faae84b9c8dee
ymca <admin@example.com>
2024-01-22 10:30:00 +0800

    Mon Jan 22 10:29:59 CST 2024  M build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@
 export JAVA_HOME=$JAVA_HOME_17_X64
 scons platform=android target=release android_arch=armv7
 scons platform=android target=release android_arch=arm64v8
+scons platform=android target=release_debug android_arch=armv7
+scons platform=android target=release_debug android_arch=arm64v8
 cd platform/android/java
 ./gradlew generateGodotTemplates
