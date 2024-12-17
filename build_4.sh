9a7bc9d205c7feebe489088f07cd2f7fbdcdb509
admin <admin@example.com>
2024-12-17 17:35:44 +0800

    Tue Dec 17 17:35:43 CST 2024  M .github/workflows/macos_editor_4.yml  M build_4.sh

--- build_4.sh
+++ build_4.sh
@@ -1,9 +1,9 @@
 #
 
 export JAVA_HOME=$JAVA_HOME_17_X64
-scons platform=android target=template_release arch=arm32 use_quickjs=yes
+# scons platform=android target=template_release arch=arm32 use_quickjs=yes
 scons platform=android target=template_release arch=arm64 use_quickjs=yes
-scons platform=android target=template_debug arch=arm32 use_quickjs=yes
+# scons platform=android target=template_debug arch=arm32 use_quickjs=yes
 scons platform=android target=template_debug arch=arm64 use_quickjs=yes
 cd platform/android/java
 ./gradlew generateGodotTemplates
