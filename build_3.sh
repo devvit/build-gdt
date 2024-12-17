f2d99ebac960ebb4d9a42c4972657eb5175fd51c
admin <admin@example.com>
2024-12-17 17:38:05 +0800

    Tue Dec 17 17:38:04 CST 2024  M build_3.sh  M build_4.sh

--- build_3.sh
+++ build_3.sh
@@ -3,12 +3,7 @@
 export JAVA_HOME=$JAVA_HOME_17_X64
 scons platform=android target=release android_arch=armv7
 scons platform=android target=release android_arch=arm64v8
-scons platform=android target=release_debug android_arch=armv7
-scons platform=android target=release_debug android_arch=arm64v8
+# scons platform=android target=release_debug android_arch=armv7
+# scons platform=android target=release_debug android_arch=arm64v8
 cd platform/android/java
 ./gradlew generateGodotTemplates
-
-# brew install --force --overwrite python@3.12
-# brew link --overwrite python@3.12
-# brew install --force --overwrite python@3.11
-# brew link --overwrite python@3.11
