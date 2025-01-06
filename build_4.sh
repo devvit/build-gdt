7de10f775677fbe7eef3203e92fee319d09be3b5
admin <admin@example.com>
2025-01-06 20:48:07 +0800

    Mon Jan  6 20:48:05 CST 2025  M .github/workflows/macos_editor_4.yml  M build_4.sh

--- build_4.sh
+++ build_4.sh
@@ -14,27 +14,24 @@ echo "version=$(git rev-parse --short HEAD)" >>$GITHUB_ENV
 
 sh misc/scripts/install_vulkan_sdk_macos.sh
 git clone --depth 1 --recursive https://github.com/mauville-technologies/godot_dragonbones modules/godot_dragonbones
-# git clone --depth 1 --recursive https://github.com/godotjs/GodotJS modules/GodotJS
+git clone --depth 1 --recursive https://github.com/godotjs/GodotJS modules/GodotJS
 git clone --depth 1 --recursive https://github.com/quinnvoker/qurobullet modules/qurobullet
 git clone --depth 1 --recursive https://github.com/limbonaut/limboai modules/limboai
-scons platform=macos arch=x86_64 target=editor
-# use_quickjs=yes
+qjs="use_quickjs=yes"
+scons platform=macos arch=x86_64 target=editor $qjs
 cp -r misc/dist/macos_tools.app ./Godot.app
 mkdir -p Godot.app/Contents/MacOS
 cp bin/godot.macos* Godot.app/Contents/MacOS/Godot
 chmod +x Godot.app/Contents/MacOS/Godot
 codesign --force --timestamp --options=runtime --entitlements misc/dist/macos/editor.entitlements -s - Godot.app
-scons platform=web target=template_release 
-# use_quickjs=yes
-# scons platform=web target=template_debug use_quickjs=yes
+scons platform=web target=template_release $qjs
+# scons platform=web target=template_debug $qjs
 
 export JAVA_HOME=$JAVA_HOME_17_X64
-scons platform=android target=template_release arch=arm32
-# use_quickjs=yes
-scons platform=android target=template_release arch=arm64 generate_apk=yes
-# use_quickjs=yes 
-# scons platform=android target=template_debug arch=arm32 use_quickjs=yes
-# scons platform=android target=template_debug arch=arm64 use_quickjs=yes
+scons platform=android target=template_release arch=arm32 $qjs
+scons platform=android target=template_release arch=arm64 generate_apk=yes $qjs
+# scons platform=android target=template_debug arch=arm32 $qjs
+# scons platform=android target=template_debug arch=arm64 $qjs
 cd platform/android/java
 ./gradlew generateGodotTemplates
 
