284683fa02e27a4ce3ec4c02a548f02fc3f3d873
admin <admin@example.com>
2025-06-08 14:44:24 +0800

    Sun Jun  8 14:44:23 CST 2025  M build_4.sh

--- build_4.sh
+++ build_4.sh
@@ -16,7 +16,7 @@ sh misc/scripts/install_vulkan_sdk_macos.sh
 git clone --depth 1 --recursive https://github.com/mauville-technologies/godot_dragonbones modules/godot_dragonbones
 # git clone --depth 1 --recursive https://github.com/godotjs/GodotJS modules/GodotJS
 git clone --depth 1 --recursive https://github.com/quinnvoker/qurobullet modules/qurobullet
-git clone --depth 1 --recursive https://github.com/limbonaut/limboai modules/limboai
+# git clone --depth 1 --recursive https://github.com/limbonaut/limboai modules/limboai
 git apply --directory modules/godot_dragonbones ../4.x_1.patch
 # qjs="use_quickjs_ng=yes"
 qjs=""
