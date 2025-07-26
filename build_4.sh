965ff10ac89ec2f7ee3050b85196c4c72088f00c
admin <admin@example.com>
2025-07-26 19:58:57 +0800

    Sat Jul 26 19:58:56 CST 2025  M .github/workflows/macos_editor_4.yml  M build_4.sh

--- build_4.sh
+++ build_4.sh
@@ -21,6 +21,8 @@ git clone --depth 1 --recursive https://github.com/limbonaut/limboai modules/lim
 git apply --directory modules/godot_dragonbones ../4.x_1.patch
 # qjs="use_quickjs_ng=yes"
 qjs=""
+
+echo 'BUILD MACOS'
 scons platform=macos arch=x86_64 target=editor $qjs
 scons platform=macos arch=arm64 target=editor $qjs
 lipo -create bin/godot.macos.editor.x86_64 bin/godot.macos.editor.arm64 -output bin/godot.macos.editor.universal
@@ -29,15 +31,20 @@ mkdir -p Godot.app/Contents/MacOS
 cp bin/godot.macos.editor.universal Godot.app/Contents/MacOS/Godot
 chmod +x Godot.app/Contents/MacOS/Godot
 codesign --force --timestamp --options=runtime --entitlements misc/dist/macos/editor.entitlements -s - Godot.app
+
+echo 'BUILD WEB'
 scons platform=web dlink_enabled=yes target=template_release $qjs
 
+echo 'BUILD ANDROID'
 export JAVA_HOME=$JAVA_HOME_17_arm64
 scons platform=android target=template_release arch=arm32 $qjs
 scons platform=android target=template_release arch=arm64 generate_apk=yes $qjs
 cd platform/android/java
 ./gradlew generateGodotTemplates
 
+echo 'PACKAGE ALL'
 cd $gd_dir
 cp godot.gdkey bin/
+ls -la bin/
 rm -rf bin/godot.macos* bin/obj
 bsdtar -czf Godot.tgz Godot.app bin/*
