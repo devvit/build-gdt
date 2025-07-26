edea8c0caa41f789a3aeda6f112181233f0865cf
admin <admin@example.com>
2025-07-26 22:39:35 +0800

    Sat Jul 26 22:39:34 CST 2025  M .github/workflows/macos_editor_4.yml A  4.x_2.patch  M build_4.sh

--- build_4.sh
+++ build_4.sh
@@ -15,7 +15,8 @@ echo "version=$(git rev-parse --short HEAD)" >>$GITHUB_ENV
 sh misc/scripts/install_vulkan_sdk_macos.sh
 git clone --depth 1 --recursive https://github.com/mauville-technologies/godot_dragonbones modules/godot_dragonbones
 # git clone --depth 1 --recursive https://github.com/godotjs/GodotJS modules/GodotJS
-# git clone --depth 1 --recursive https://github.com/quinnvoker/qurobullet modules/qurobullet
+git clone --depth 1 --recursive https://github.com/quinnvoker/qurobullet modules/qurobullet
+git apply --directory modules/qurobullet ../4.x_2.patch
 # git clone --depth 1 --recursive https://github.com/Zylann/godot_voxel modules/voxel
 # git clone --depth 1 --recursive https://github.com/limbonaut/limboai modules/limboai
 git apply --directory modules/godot_dragonbones ../4.x_1.patch
