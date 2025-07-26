63e02f5921cc057f5093ab19d2f89a50f24c09ab
admin <admin@example.com>
2025-07-26 21:51:45 +0800

    Sat Jul 26 21:51:43 CST 2025  M .github/workflows/macos_editor_4.yml  M build_3.sh  M build_4.sh

--- build_3.sh
+++ build_3.sh
@@ -22,6 +22,8 @@ mv godot_dragonbones* modules/godot_dragonbones
 perl -pi -e 's/-fno-rtti//g' platform/android/detect.py
 perl -pi -e 's/-fno-rtti//g' platform/javascript/detect.py
 git apply --directory modules/godot_dragonbones ../3.x_1.patch
+
+echo 'BUILD MACOS'
 scons platform=osx arch=x86_64 target=release_debug tools=yes
 scons platform=osx arch=arm64 target=release_debug tools=yes
 lipo -create bin/godot.osx.opt.tools.x86_64 bin/godot.osx.opt.tools.arm64 -output bin/godot.osx.tools.universal
@@ -29,15 +31,20 @@ cp -r misc/dist/osx_tools.app Godot.app
 mkdir -p Godot.app/Contents/MacOS
 cp bin/godot.osx.tools.universal Godot.app/Contents/MacOS/Godot
 chmod +x Godot.app/Contents/MacOS/Godot
+
+echo 'BUILD WEB'
 scons platform=javascript tools=no target=release LINKFLAGS='-sGL_ENABLE_GET_PROC_ADDRESS'
 
+echo 'BUILD ANDROID'
 export JAVA_HOME=$JAVA_HOME_17_arm64
 scons platform=android target=release android_arch=armv7
 scons platform=android target=release android_arch=arm64v8
 cd platform/android/java
 ./gradlew generateGodotTemplates
 
+echo 'PACKAGE ALL'
 cd $gd_dir
 cp godot.gdkey bin/
+ls -la bin/
 rm -rf bin/godot.osx*
 bsdtar -czf Godot.tgz Godot.app bin/*
