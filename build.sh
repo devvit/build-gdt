aba0450a8d3e18ab4eac73b48adddb953b52b56d
admin <admin@example.com>
2026-01-21 15:41:49 +0800

    Wed Jan 21 15:41:48 CST 2026  M .github/workflows/4_macos.yml  M build.sh

--- build.sh
+++ build.sh
@@ -13,27 +13,24 @@ export OSXCROSS_ROOT=/osxcross
 
 git clone --depth 1 --recursive https://github.com/godotengine/godot
 cd godot
-gd_dir=$(pwd)
-
-echo $gd_dir
 
 openssl rand -hex 32 >godot.gdkey
-ls
-export SCRIPT_AES256_ENCRYPTION_KEY=$(cat godot.gdkey)
+export SCRIPT_AES256_ENCRYPTION_KEY=\$(cat godot.gdkey)
 
 echo 'BUILD MACOS'
 scons platform=macos arch=x86_64 target=editor
 cp -r misc/dist/macos_tools.app ./Godot.app
-mkdir -p Godot.app/Contents/MACOS
+mkdir -p Godot.app/Contents/MacOS
 cp bin/godot.macos.editor.x86_64 Godot.app/Contents/MacOS/Godot
 chmod +x Godot.app/Contents/MacOS/Godot
 codesign --force --timestamp --options=runtime --entitlements misc/dist/macos/editor.entitlements -s - Godot.app
 
 echo 'PACKAGE ALL'
-cd $gd_dir
 cp godot.gdkey bin/
 ls -la bin/
 rm -rf bin/godot.macos* bin/obj
 bsdtar -czf Godot.tgz Godot.app bin/*
-mv Godot.tgz /build
 "
+
+cd godot
+echo "version=$(git rev-parse --short HEAD)" >>$GITHUB_ENV
