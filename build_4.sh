dfa7caf66c12a0c28602531f8068281dfcf113ca
admin <admin@example.com>
2026-08-20 17:07:51 +0800

    Thu Aug 20 17:07:50 CST 2026  M build_4.sh

--- build_4.sh
+++ build_4.sh
@@ -13,14 +13,14 @@ export SCRIPT_AES256_ENCRYPTION_KEY=$(cat godot.gdkey)
 echo "version=$(git rev-parse --short HEAD)" >>$GITHUB_ENV
 
 sh misc/scripts/install_vulkan_sdk_macos.sh
-git clone --depth 1 --recursive https://github.com/mauville-technologies/godot_dragonbones modules/godot_dragonbones
-git clone --depth 1 --recursive https://github.com/quinnvoker/qurobullet modules/qurobullet
-git apply --directory modules/qurobullet ../4.x_2.patch
-git clone --depth 1 --recursive https://github.com/Zylann/godot_voxel modules/voxel
-git clone --depth 1 --recursive https://github.com/limbonaut/limboai modules/limboai
-git apply --directory modules/godot_dragonbones ../4.x_1.patch
-git clone --depth 1 --recursive https://github.com/gd-avif/gd-avif modules/avif
-perl -pi -e 's/#include "core\/extension\/ext_wrappers\.gen\.inc"/#include "core\/extension\/ext_wrappers.gen.h"/' modules/avif/resource_saver_avif.h
+# git clone --depth 1 --recursive https://github.com/mauville-technologies/godot_dragonbones modules/godot_dragonbones
+# git clone --depth 1 --recursive https://github.com/quinnvoker/qurobullet modules/qurobullet
+# git apply --directory modules/qurobullet ../4.x_2.patch
+# git clone --depth 1 --recursive https://github.com/Zylann/godot_voxel modules/voxel
+# git clone --depth 1 --recursive https://github.com/limbonaut/limboai modules/limboai
+# git apply --directory modules/godot_dragonbones ../4.x_1.patch
+# git clone --depth 1 --recursive https://github.com/gd-avif/gd-avif modules/avif
+# perl -pi -e 's/#include "core\/extension\/ext_wrappers\.gen\.inc"/#include "core\/extension\/ext_wrappers.gen.h"/' modules/avif/resource_saver_avif.h
 git clone --depth 1 --recursive https://github.com/libriscv/godot-sandbox modules/sandbox
 # git clone --depth 1 --recursive https://github.com/godotjs/GodotJS modules/GodotJS
 # qjs="use_quickjs_ng=yes"
