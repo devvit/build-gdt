1a55e1fdc3b884c9f27347d1240a87331854db6a
admin <admin@example.com>
2026-08-20 11:35:53 +0800

    Thu Aug 20 11:35:52 CST 2026  M build_4.sh

--- build_4.sh
+++ build_4.sh
@@ -20,11 +20,14 @@ git clone --depth 1 --recursive https://github.com/Zylann/godot_voxel modules/vo
 git clone --depth 1 --recursive https://github.com/limbonaut/limboai modules/limboai
 git apply --directory modules/godot_dragonbones ../4.x_1.patch
 git clone --depth 1 --recursive https://github.com/gd-avif/gd-avif modules/avif
-perl -i -ne 'print unless /GDEXTENSION/' modules/avif/SConstruct
+cd modules/avif
+scons platform=macos arch=x86_64 target=template_release
 # git clone --depth 1 --recursive https://github.com/libriscv/godot-sandbox modules/sandbox
 # git clone --depth 1 --recursive https://github.com/godotjs/GodotJS modules/GodotJS
 # qjs="use_quickjs_ng=yes"
 qjs=""
+
+cd $gd_dir
 
 echo 'BUILD MACOS'
 scons platform=macos arch=x86_64 target=editor $qjs
