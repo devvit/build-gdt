9e2ace691acfb00be76b7e6bf7e8559fb605d65d
admin <admin@example.com>
2026-08-20 17:07:27 +0800

    Thu Aug 20 17:07:26 CST 2026  M .github/workflows/macos_editor_4.yml  M build_4.sh

--- build_4.sh
+++ build_4.sh
@@ -21,7 +21,7 @@ git clone --depth 1 --recursive https://github.com/limbonaut/limboai modules/lim
 git apply --directory modules/godot_dragonbones ../4.x_1.patch
 git clone --depth 1 --recursive https://github.com/gd-avif/gd-avif modules/avif
 perl -pi -e 's/#include "core\/extension\/ext_wrappers\.gen\.inc"/#include "core\/extension\/ext_wrappers.gen.h"/' modules/avif/resource_saver_avif.h
-# git clone --depth 1 --recursive https://github.com/libriscv/godot-sandbox modules/sandbox
+git clone --depth 1 --recursive https://github.com/libriscv/godot-sandbox modules/sandbox
 # git clone --depth 1 --recursive https://github.com/godotjs/GodotJS modules/GodotJS
 # qjs="use_quickjs_ng=yes"
 qjs=""
