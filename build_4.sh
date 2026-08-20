33f326d8d9dd424857825be716c8a6d09dc7ddc3
admin <admin@example.com>
2026-08-20 10:13:01 +0800

    Thu Aug 20 10:13:00 CST 2026  M build_4.sh

--- build_4.sh
+++ build_4.sh
@@ -20,6 +20,7 @@ git clone --depth 1 --recursive https://github.com/Zylann/godot_voxel modules/vo
 git clone --depth 1 --recursive https://github.com/limbonaut/limboai modules/limboai
 git apply --directory modules/godot_dragonbones ../4.x_1.patch
 git clone --depth 1 --recursive https://github.com/gd-avif/gd-avif modules/avif
+rm -rf modules/avif/SConstruct
 # git clone --depth 1 --recursive https://github.com/libriscv/godot-sandbox modules/sandbox
 # git clone --depth 1 --recursive https://github.com/godotjs/GodotJS modules/GodotJS
 # qjs="use_quickjs_ng=yes"
