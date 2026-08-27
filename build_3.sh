07578211f462791516740350a0de96fef9e84b36
admin <admin@example.com>
2026-08-27 09:21:05 +0800

    Thu Aug 27 09:21:04 CST 2026  M .github/workflows/macos_editor_3.yml  M build_3.sh

--- build_3.sh
+++ build_3.sh
@@ -18,29 +18,30 @@ rm -rf godot_dragonbones*.zip
 mv godot_dragonbones* modules/godot_dragonbones
 # git clone --depth 1 -b 3.4 --recursive https://github.com/godotjs/javascript modules/ECMAScript
 # git apply --directory modules/ECMAScript ../3.x_2.patch
-# git clone --depth 1 -b godot-3.x --recursive https://github.com/quinnvoker/qurobullet modules/qurobullet
+git clone --depth 1 -b godot-3.x --recursive https://github.com/quinnvoker/qurobullet modules/qurobullet
 perl -pi -e 's/-fno-rtti//g' platform/android/detect.py
 perl -pi -e 's/-fno-rtti//g' platform/javascript/detect.py
 git apply --directory modules/godot_dragonbones ../3.x_1.patch
 
 echo 'BUILD MACOS'
 scons platform=osx arch=x86_64 target=release_debug tools=yes
-scons platform=osx arch=arm64 target=release_debug tools=yes
-lipo -create bin/godot.osx.opt.tools.x86_64 bin/godot.osx.opt.tools.arm64 -output bin/godot.osx.tools.universal
+# scons platform=osx arch=arm64 target=release_debug tools=yes
+# lipo -create bin/godot.osx.opt.tools.x86_64 bin/godot.osx.opt.tools.arm64 -output bin/godot.osx.tools.universal
 cp -r misc/dist/osx_tools.app Godot.app
 mkdir -p Godot.app/Contents/MacOS
-cp bin/godot.osx.tools.universal Godot.app/Contents/MacOS/Godot
+# cp bin/godot.osx.tools.universal Godot.app/Contents/MacOS/Godot
+cp bin/godot.osx.opt.tools.x86_64 Godot.app/Contents/MacOS/Godot
 chmod +x Godot.app/Contents/MacOS/Godot
 
-echo 'BUILD WEB'
-scons platform=javascript tools=no target=release LINKFLAGS='-sGL_ENABLE_GET_PROC_ADDRESS'
+# echo 'BUILD WEB'
+# scons platform=javascript tools=no target=release LINKFLAGS='-sGL_ENABLE_GET_PROC_ADDRESS'
 
-echo 'BUILD ANDROID'
-export JAVA_HOME=$JAVA_HOME_17_arm64
-scons platform=android target=release android_arch=armv7
-scons platform=android target=release android_arch=arm64v8
-cd platform/android/java
-./gradlew generateGodotTemplates
+# echo 'BUILD ANDROID'
+# export JAVA_HOME=$JAVA_HOME_17_arm64
+# scons platform=android target=release android_arch=armv7
+# scons platform=android target=release android_arch=arm64v8
+# cd platform/android/java
+# ./gradlew generateGodotTemplates
 
 echo 'PACKAGE ALL'
 cd $gd_dir
